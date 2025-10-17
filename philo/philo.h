8bea02c98b5ee86409af685ca8add0560deeaecc
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2025-10-17 15:58:55 +0300

    feat: set up file structure, created philo struct and simulation data struct

---
+++ philo/philo.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 11:37:49 by nmascaro          #+#    #+#             */
+/*   Updated: 2025/10/17 15:26:43 by nmascaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_simulation_data
+{
+	int	philos_num;
+	int	time_to_die; //milliseconds
+	int	time_to_eat; // milliseconds
+	int	time_to_sleep; // milliseconds
+	int	must_eat_count; //optional argument, if not specified, simulation stops when a philo dies
+	long long	starting_time; // milliseconds
+	pthread_mutex_t	*fork; //array of mutexes
+	pthread_mutex_t	mutex_print; //mutex to avoid overlapping output messages
+	int	stop_simulation;
+}	t_simulation_data;
+
+typedef struct s_philo
+{
+	int	id;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t	*right_fork;
+	long long	time_of_last_eat;
+	int times_eaten;
+	t_simulation_data *data;
+}	t_philo;
+
+#endif
